6d13b41b88aa429e954bc588400085386f2df4c2
Trung V. Dang <viettrungdang@gmail.com>
2018-04-22 11:39:52 +0900

    tree

--- client/src/components/Tree.js
+++ client/src/components/Tree.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import SchoolIcon from 'material-ui-icons/School';
+import WorkIcon from 'material-ui-icons/Work';
 
 class Tree extends Component {
+  state = {
+    timeline: []
+  }
+  componentDidMount() {
+    const timeline = require('../db/timeline')
+    this.setState({ timeline })
+  }
+
   render() {
-    return (
-      <div className="Tree">
-        <p>Tree here</p>
-      </div>
-    );
+    return <div style={{height: '100%'}}>
+      <VerticalTimeline>
+        {this.state.timeline.map((event) => (
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date={event.date}
+            iconStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
+            icon={<WorkIcon/>}
+          >
+            <p>
+              {event.content}
+            </p>
+          </VerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
+    </div>;
   }
 }
 
